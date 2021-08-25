d3d00ee75725d021c24d529f0879e0ceb56fb82d
Halvor Groven <halgro20@gmail.com>
2021-08-25 22:50:58 +0200

    Remove old stuff

--- code/traci-script.py
+++ code/traci-script.py
@@ -28,32 +28,3 @@ while traci.simulation.getMinExpectedNumber() > 0 and step < 1000:
 handler.simulation_end = True
 traci.close(False)
 
-
-
-
-
-
-
-
-
-# TODO:
-# Implement Voltage check on batteries, if below a certain level, RTL
-# 1. If new vehicles but no new drones, dont try to create thread.
-# 2. Ensure RTL mode with while loop if drone is told to go outside geofence or over max altidtude
-# 3. Look more into overshooting and undershooting
-# 4. Some parameters must be tweked, i.e how long the drone waits to goto next waypoint, or how long it takes for the drone to check if there is a new location
-# Maybe let the drone check the angle between current location and next point. It can skip all points that is in a direct line.
-# - a = angle_to_next point
-# while angle_delta < some_degrees
-# Location = LocationQueue.get()
-# simple_goto(Location)
-
-# 5. Time synchronisation is a hard task and meant calls for further research. Now every drone has to wait for a new
-# drone to enter the network at it's starting location, and vehicles may also not be able to keep up correct speed
-# and acceleration. This will affect the reflection of the simulation.
-# 6. Buffer overflow
-#KNOWN BUGS
-#-The syncing does not work if the simulation is finished before all drones hav reached their starting points.
-#-The vehicles does not enter RTL moode iff they finish in a straigh line and the last point is sent to the drone before the simulation is finished.
-#This is due to the blocking behavior of self.locationqueue.get() on line 134 in Vehicle.pyø
-
